ff3139617a699fc5fcc310f7a68dc0f2412d528b
10ion <71271713+Ian-Ceron@users.noreply.github.com>
2021-05-17 12:43:31 -0600

    Splash stuff

--- Spirit of Collapse/Assets/Environment/OtherCode/SplashScreens.cs
+++ Spirit of Collapse/Assets/Environment/OtherCode/SplashScreens.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SplashScreens : MonoBehaviour
 {
+    private bool ButtonDown;
+    private Image sr;
+    public GameObject images;
+    public SceneManager NextScene;
+    private bool HasBeenPressed;
     // Start is called before the first frame update
     void Start()
     {
+        sr = GetComponent<Image>();
         
     }
 
@@ -14,5 +22,25 @@ public class SplashScreens : MonoBehaviour
     void Update()
     {
         
+        ButtonDown = Input.anyKeyDown;
+        if (ButtonDown == true || HasBeenPressed == true)
+        {
+            HasBeenPressed = true;
+
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a - 0.5f * Time.deltaTime);
+            if (sr.color.a < 1)
+            {
+                this.gameObject.SetActive(false);
+                if (images != null)
+                {
+                    images.SetActive(true);
+                }
+                else
+                {
+                    SceneManager.LoadScene(1);
+                }
+            }
+        }
+        else { sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a + 0.5f * Time.deltaTime); }     
     }
 }
