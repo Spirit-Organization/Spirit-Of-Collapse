935b1cb8bc85a52c6e9a793f6abc800374c63086
Elijah Lawson <89939961+Elijah5123220@users.noreply.github.com>
2021-10-27 14:20:31 -0600

    watergrowth Changes

--- Spirit of Collapse/Assets/movingplatform.cs
+++ Spirit of Collapse/Assets/movingplatform.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class movingplatform : MonoBehaviour
 {
+
+    public GameObject platform;
+    public float moveSpeed;
+    public Transform currentpoint;
+    public Transform[] points;
+    public int pointSelection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentpoint = points[pointSelection];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, currentpoint.position, Time.deltaTime * moveSpeed);
+
+        if(platform.transform.position == currentpoint.position)
+        {
+            pointSelection++;
+        }
+
+        if(pointSelection == points.Length)
+        {
+            pointSelection = 0;
+        }
+    }    
 }
